55209c5edf54e715a895ec6a565c39d64262ab11
Roosevelt Burden <rooseveltburden@gmail.com>
2024-12-02 20:14:12 -0700

    [OT] updated `print_results`

--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -2,16 +2,15 @@ use console::style;
 
 use std::fmt::Debug;
 
-pub fn print_results<T: Debug>(day: usize, part_1: T, part_2: T) {
-    let style_part = |num: usize, part: T| {
+pub fn print_results<T: Debug>(day: usize, parts: &[T]) {
+    let style_part = |(num, part): (usize, &T)| {
         format!(
             "{}: {:?}",
-            style(format!("[Part {num}]")).magenta().bold(),
+            style(format!("\n\t[Part {}]", num + 1)).magenta().bold(),
             style(part).bold()
         )
     };
     let day = style(format!("[Day {day}]")).yellow().bold();
-    let p1 = style_part(1, part_1);
-    let p2 = style_part(2, part_2);
-    print!("\n{day}:\n\t{p1}\n\t{p2}\n")
+    let parts: String = parts.iter().enumerate().map(style_part).collect();
+    print!("\n{day}{parts}\n")
 }
